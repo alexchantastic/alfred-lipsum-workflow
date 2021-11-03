3dd57034e49f891acd75a4fc3ee0a9c6242dabf5
Alex Chan <alexchantastic@gmail.com>
2021-11-02 17:51:27 -0700

    Add get_character

--- lipsum.py
+++ lipsum.py
@@ -5,9 +5,17 @@ args = sys.argv
 type = args[1]
 count = int(args[2]) if len(args) >= 3 else 1
 
+def get_character(count):
+  pool = lorem.get_word(1)
+
+  while len(pool) <= count:
+    pool += ' ' + lorem.get_word(1)
+
+  return pool[0:count]
+
 def lipsum(type):
   if type == 'characters':
-    return ''
+    return get_character(count).capitalize()
 
   elif type == 'words':
     return lorem.get_word(count).capitalize()
