a0d3574c38c7c7254fe8f5addcbb1f57d006b874
Alex Chan <alexchantastic@gmail.com>
2022-03-20 14:07:01 -0700

    Migrate to lipsum-shell

---
+++ lipsum.sh
@@ -0,0 +1,190 @@
+#!/bin/sh
+version="1.0.0"
+word_pool=("ad" "adipiscing" "aliqua" "aliquip" "amet" "anim" "aute" "cillum" "commodo"
+         "consectetur" "consequat" "culpa" "cupidatat" "deserunt" "do" "dolor" "dolore"
+         "duis" "ea" "eiusmod" "elit" "enim" "esse" "est" "et" "eu" "ex" "excepteur"
+         "exercitation" "fugiat" "id" "in" "incididunt" "ipsum" "irure" "labore" "laboris"
+         "laborum" "lorem" "magna" "minim" "mollit" "nisi" "non" "nostrud" "nulla"
+         "occaecat" "officia" "pariatur" "proident" "qui" "quis" "reprehenderit" "sed"
+         "sint" "sit" "sunt" "tempor" "ullamco" "ut" "velit" "veniam" "voluptate")
+default_min_words=4
+default_max_words=8
+         
+# Generate a series of characters
+#
+# @param  int $count  Number of characters to generate
+# @return string
+generate_characters() {
+  local count word words characters
+
+  count=$1
+
+  while [ ${#characters} -lt $count ]
+  do
+    word=$(generate_words 1)
+    words+=($word)
+    characters=${words[@]}
+  done
+
+  echo ${characters:0:$count}
+}
+
+# Generate a series of words
+#
+# @param  int $count  Number of words to generate
+# @return string
+generate_words() {
+  local i count rand word words
+
+  count=$1
+
+  for ((i=0; i<$count; i++))
+  do
+    rand=$(( RANDOM % ${#word_pool[*]} ))
+    word=${word_pool[$rand]}
+    words+=($word)
+  done
+
+  echo ${words[@]}
+}
+
+# Generate a series of sentences
+#
+# @param  int $count    Number of sentences to generate
+# @param  int $min      Minimum number of words per sentence (default: 4)
+# @param  int $max      Maximum number of words per sentence (default: 8)
+# @return string
+generate_sentences() {
+  local i count min max len placement sentence sentences
+
+  count=$1
+  min=${2:-$default_min_words}
+  max=${3:-$default_max_words}
+
+  for ((i=0; i<$count; i++))
+  do
+    len=$(( RANDOM % (${max} - ${min} + 1 ) + ${min} ))
+    sentence=$(generate_words $len)
+    sentence=$(capitalize "$sentence")
+
+    # Randomly distribute commas throughout the sentences
+    if [ $len -gt 1 -a $(( RANDOM % 2 )) = 1 ]; then
+      placement=$(( RANDOM % ($len - 1) + 1 ))
+      sentence=$(sed "s/ /, /"$placement <<<$sentence)
+    fi
+
+    sentences+=($sentence.)
+  done
+
+  echo ${sentences[@]}
+}
+
+# Generate a series of paragraphs
+#
+# @param  int $count        Number of paragraphs to generate
+# @param  int $min          Minimum number of sentences per paragraph (default: 5)
+# @param  int $max          Maximum number of sentences per paragraph (default: 10)
+# @param  int $min_words    Minimum number of words per sentence (default: 4)
+# @param  int $max_words    Maximum number of words per sentence (default: 8)
+# @return string
+generate_paragraphs() {
+  local i count min max len min_words max_words paragraph paragraphs
+
+  count=$1
+  min=${2:-5}
+  max=${3:-10}
+  min_words=${4:-$default_min_words}
+  max_words=${5:-$default_max_words}
+
+  for ((i=0; i<$count; i++))
+  do
+    len=$(( RANDOM % (${max} - ${min} + 1 ) + ${min} ))
+    paragraph=$(generate_sentences $len $min_words $max_words)
+    paragraphs+=$paragraph
+
+    # Add linebreaks
+    if [ ! $i = $(( $count - 1 )) ]; then
+      paragraphs+="\n\n"
+    fi
+  done
+
+  echo ${paragraphs[@]}
+}
+
+# Capitalize the first letter in a string
+#
+# @param  string $string    String to capitalize
+# @return string
+capitalize() {
+  local string first_letter ord
+
+  string=$1
+
+  first_letter=${string:0:1}
+
+  if [[ ${first_letter} == [a-z] ]]; then
+    ord=$(printf '%o' "'${first_letter}")
+    ord=$(( ord - 40 ))
+    first_letter=$(printf '%b' '\'${ord})
+  fi
+
+  echo "${first_letter}${string:1}"
+}
+
+# Show help information
+#
+# @return string
+show_help() {
+  echo "Generates lorem ipsum dummy text"
+  echo
+  echo "Usage: lipsum [-t] [-c] [-m|M|w|W|h|v]"
+  echo "options:"
+  echo "  t    Type of text structure to generate (characters|words|sentences|paragraphs)"
+  echo "  c    Number of structures to generate"
+  echo "  m    Minimum number of structures to generate (applies to sentence and paragraph only)"
+  echo "  M    Maximum number of structures to generate (applies sentence and paragraph only)"
+  echo "  w    Minimum number of sentence words to generate (applies to paragraph only)"
+  echo "  W    Maximum number of sentences words to generate (applies to paragraph only)"
+  echo "  v    Print the software version"
+  echo "  h    Print help"
+}
+
+# Show version information
+#
+# @return string
+show_version() {
+  echo "lipsum $version"
+}
+
+while getopts t:c:m:M:w:W:hv option
+do
+  case $option in
+    t) type=${OPTARG};;
+    c) count=${OPTARG};;
+    m) min=${OPTARG};;
+    M) max=${OPTARG};;
+    w) min_words=${OPTARG};;
+    W) max_words=${OPTARG};;
+    h) show_help;;
+    v) show_version;;
+    *)
+        echo "Incorrect options provided"
+        exit 1
+        ;;
+  esac
+done
+
+case $type in
+  characters|character|char|c)
+    printf "$(capitalize "$(generate_characters $count)")"
+    ;;
+  words|word|w)
+    printf "$(capitalize "$(generate_words $count)")"
+    ;;
+  sentences|sentence|sent|s)
+    printf "$(generate_sentences $count $min $max)"
+    ;;
+  paragraphs|paragraph|para|p)
+    printf "$(generate_paragraphs $count $min $max $min_words $max_words)"
+    ;;
+esac
